142cbdf78c7a73827acf40768e3d01e16dcf6b85
SomaSekimoto <0421.soma@gmail.com>
2022-06-12 17:59:06 +0900

    mint BobToken as interest when deposit AliceToken

--- task1/contracts/BobToken.sol
+++ task1/contracts/BobToken.sol
@@ -8,17 +8,20 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-// import "./AliceToken.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract BobToken is ERC20, Ownable {
+    using SafeMath for uint256;
     using Address for address;
-    // using SafeMath for uint256; // As of Solidity v0.8.0, mathematical operations can be done safely without the need for SafeMath
     using SafeERC20 for IERC20;
     IERC20 public token;
+    uint256 private interest;
 
-    constructor(address _token) public ERC20("BobToken", "BBT") {
+    uint256 public interestRate;
+
+    constructor(address _token, uint256 rate) public ERC20("BobToken", "BBT") {
         token = IERC20(_token);
+        setInterestRate(rate);
     }
 
     // constructor(uint256 totalSupply) public ERC20("BobToken", "BBT") {
@@ -36,16 +39,10 @@ contract BobToken is ERC20, Ownable {
         require(_amount > 0, "amount cannot be 0");
 
         // Transfer AliceToken to smart contract
-        console.log(msg.sender);
-        console.log(address(this));
-        console.log(_amount);
-
-        console.log(address(token));
-        console.log(token.balanceOf(msg.sender));
         token.safeTransferFrom(msg.sender, address(this), _amount);
-
         // Mint BobToken to msg sender
-        _mint(msg.sender, _amount);
+        interest = _amount.mul(interestRate) / 10**18;
+        _mint(msg.sender, interest);
     }
 
     function withdraw(uint256 _amount) public {
@@ -54,5 +51,9 @@ contract BobToken is ERC20, Ownable {
 
         // Transfer MyTokens from this smart contract to msg sender
         token.safeTransfer(msg.sender, _amount);
+    }
+
+    function setInterestRate(uint256 _rate) public onlyOwner {
+        interestRate = _rate;
     }
 }
