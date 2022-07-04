45cc7d75e90e3ae71ba75c9814c42ef3f4bcda73
SomaSekimoto <0421.soma@gmail.com>
2022-07-04 14:08:32 +0900

    deploy: on goerli through  alchemy

--- task1/hardhat.config.js
+++ task1/hardhat.config.js
@@ -13,7 +13,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+task("envs", "Prints the list of accounts", async (taskArgs, hre) => {
+  let envs = [API_URL, API_KEY, PRIVATE_KEY];
+  envs.forEach((e) => console.log(e));
+});
 
 module.exports = {
   solidity: "0.8.1",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
